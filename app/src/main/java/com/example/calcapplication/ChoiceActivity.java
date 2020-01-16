fce5e1b188838433364c1a5ec35500ee7e5c6a7f
xarekx <arektarnowski4@gmail.com>
2020-01-16 11:34:29 +0100

    Create intents and 2 activities ( geometry, function )

--- app/src/main/java/com/example/calcapplication/ChoiceActivity.java
+++ app/src/main/java/com/example/calcapplication/ChoiceActivity.java
@@ -1,14 +1,19 @@
 package com.example.calcapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class ChoiceActivity extends AppCompatActivity {
 
     private final String TAG = "ChoiceActivity";
     private TextView mChoiceTextView;
+    private CardView mGeometryCardView;
+    private CardView mFunctionCardView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,28 @@ public class ChoiceActivity extends AppCompatActivity {
                 "\nWhat would you like to calc?";
 
         mChoiceTextView = findViewById(R.id.choice_tv);
+        mGeometryCardView = findViewById(R.id.geometry_card);
+        mFunctionCardView = findViewById(R.id.function_card);
+
+        mGeometryCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),GeometricActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mFunctionCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FunctionActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         mChoiceTextView.setText(choicePage);
+
+
     }
 }

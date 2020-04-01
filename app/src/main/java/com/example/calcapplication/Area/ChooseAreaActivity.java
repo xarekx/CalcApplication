8fbd2c5f347de62bbb2b1d1774da21074f67f610
xarekx <arektarnowski4@gmail.com>
2020-04-01 08:48:08 +0200

    Create GeometricLayout, create Area, Volume Activity

---
+++ app/src/main/java/com/example/calcapplication/Area/ChooseAreaActivity.java
@@ -0,0 +1,16 @@
+package com.example.calcapplication.Area;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.calcapplication.R;
+
+public class ChooseAreaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_area);
+    }
+}
--- app/src/main/java/com/example/calcapplication/GeometricActivity.java
+++ app/src/main/java/com/example/calcapplication/GeometricActivity.java
@@ -6,9 +6,14 @@ import android.os.Bundle;
 
 public class GeometricActivity extends AppCompatActivity {
 
+    private final String TAG = "Geometric Activity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geometric);
     }
+
+
+
 }
